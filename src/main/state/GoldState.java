58ebf5b47367aaf1d3102d790f636d25f2681b6a
ChristopheKunzli <christophe.kunzli@heig-vd.ch>
2025-03-19 15:32:08 +0100

    add 2 other states

---
+++ src/main/state/GoldState.java
@@ -0,0 +1,19 @@
+package main.state;
+
+import main.Account;
+
+public class GoldState extends State {
+    protected GoldState(Account account) {
+        super(account);
+    }
+
+    @Override
+    public void deposit(int amount) {
+
+    }
+
+    @Override
+    public void withdraw(int amount) {
+
+    }
+}
---
+++ src/main/state/PlatinumState.java
@@ -0,0 +1,19 @@
+package main.state;
+
+import main.Account;
+
+public class PlatinumState extends State {
+    protected PlatinumState(Account account) {
+        super(account);
+    }
+
+    @Override
+    public void deposit(int amount) {
+
+    }
+
+    @Override
+    public void withdraw(int amount) {
+
+    }
+}
